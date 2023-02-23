ec9614579962ebd472d691f5f5636f9280158cc2
Prabhu <prabh.rao@gmail.com>
2023-02-23 14:28:23 +0530

    vars

---
+++ 04-vars/var.tf
@@ -0,0 +1,23 @@
+variable "sample" {
+  default = 100
+}
+
+output "sample" {
+  value = var.sample
+}
+
+variable "sample1" {}
+
+output "sample1" {
+  value = var.sample1
+}
+
+variable "cli" {}
+
+output "cli" {
+  value = var.cli
+}
+
+variable "input" {}
+
+
