c5ef33e9d032dcb58224080586633fac8c4e5c66
Prabhu <prabh.rao@gmail.com>
2023-02-24 19:09:42 +0530

    test py

---
+++ 01.sample/test1.py
@@ -0,0 +1,12 @@
+#age=int(input("please enter your age"))
+#if age >= 18:
+#   print("you are eligible for vote")
+#else:
+#   print("you are not eligible for vote")
+
+a = 1
+b = 3
+if a > b:
+    print("a is Greater")
+else:
+    print("b is Greater")
