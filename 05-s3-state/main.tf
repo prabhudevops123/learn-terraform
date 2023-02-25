436b7170d4003e70b33aca0d12dd48da0dff6890
Prabhu <prabh.rao@gmail.com>
2023-02-25 22:00:56 +0530

    DNS

--- 05-s3-state/main.tf
+++ 05-s3-state/main.tf
@@ -1,3 +1,20 @@
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-prabhu"
+#    key    = "05-s3-state/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
+#
+#resource "aws_instance" "ec2" {
+#  ami                    = "ami-0a017d8ceb274537d"
+#  instance_type          = "t3.micro"
+#  vpc_security_group_ids = ["sg-0ab98190dda1422c4"]
+#  tags = {
+#    Name = "test"
+#  }
+#}
+
 terraform {
   backend "s3" {
     bucket = "terraform-prabhu"
@@ -9,8 +26,9 @@ terraform {
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0ab98190dda1422c4"]
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
   tags = {
-    Name = "test"
+    Name = "demo"
   }
 }
+
