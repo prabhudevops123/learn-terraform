92881c79999de5182dd798122eeb7d0ca68d7dd6
Prabhu <prabh.rao@gmail.com>
2023-03-01 14:14:44 +0530

    terraform modules

--- roboshop-setup/ex4/ec2/main.tf
+++ roboshop-setup/ex4/ec2/main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "ec2" {
 
 resource "aws_route53_record" "record" {
   zone_id = "Z04913851JPF0HITS640T"
-  name    = "${var.component}-dev.devopsb71.online"
+  name    = "${var.component}-dev.devopsprabhu.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.ec2.private_ip]
