6f9188cc865bbcab5b665de36b41c07aac2dabae
Prabhu <prabh.rao@gmail.com>
2023-03-09 00:04:04 +0530

    roboshop-ansible parameters secrets

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -15,7 +15,8 @@ resource "aws_spot_instance_request" "node1" {
 #  value       = "prom-test-server"
 #}
 
-resource "aws_spot_instance_ami-0a017d8ceb274537d"
+resource "aws_spot_instance_request" "node2" {
+  ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0ab98190dda1422c4"]
   wait_for_fulfillment   = true
