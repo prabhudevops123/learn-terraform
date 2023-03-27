82a4d99e92eb2bf0e3c15673c4f5f42284508f07
Prabhu <prabh.rao@gmail.com>
2023-03-27 13:36:07 +0530

    roboshop docdb jenkins

--- 01.sample/test.tf
+++ 01.sample/test.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "ec2" {
-  ami                    = "ami-0a017d8ceb274537d"
+  ami                    = "ami-0089b8e98cd95257d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0ab98190dda1422c4"]
   tags = {
